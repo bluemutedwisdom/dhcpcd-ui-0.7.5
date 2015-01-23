ab2fe553a51ffccd6698b22056b2e779ab2795e2
spl <simon@raspberrypi.org>
2015-01-23 10:40:51 +0000

    Scans made to happen when an interface is not connected.

--- src/dhcpcd-gtk/main.c
+++ src/dhcpcd-gtk/main.c
@@ -683,13 +683,12 @@ bgscan(gpointer data)
 
 	con = (DHCPCD_CONNECTION *)data;
 	TAILQ_FOREACH(w, &wi_scans, next) {
-		if (w->interface->wireless && w->interface->up) {
+		if (w->interface->wireless) {
 			wpa = dhcpcd_wpa_find(con, w->interface->ifname);
 			if (wpa)
 				dhcpcd_wpa_scan(wpa);
 		}
 	}
-
 	return TRUE;
 }
 #endif
